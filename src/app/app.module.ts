3ebb7a72d1e7a3cb9babb655a6c3892eb1564514
Aleksander Przybylo <tchebb@yahoo.ca>
2020-12-11 09:44:20 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+import { RouterModule, Routes } from '@angular/router'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AddArticleComponent } from './components/add-article/add-article.component';
+import { ArticleDetailsComponent } from './components/article-details/article-details.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+
+const routes: Routes = []
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddArticleComponent,
+    ArticleDetailsComponent,
+    ArticleListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
